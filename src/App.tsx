edd44f3a4f0e010573381cd8ab1d39313bb9f9b3
Rizwan21-git <therizwantechworks@gmail.com>
2025-12-26 22:26:31 +0500

    Upated UI to light

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React from "react";
-import TallyWindow from "./Components/Pages/TallyWindow";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<TallyWindow />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+import React from 'react'
+import './index.css'
+import Home from './Pages/Home';
 
-export default App;
+function App() {
+  return (
+      <Home/>
+  );
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial'],
+      },
+    },
   },
   plugins: [],
-};
+}
+
